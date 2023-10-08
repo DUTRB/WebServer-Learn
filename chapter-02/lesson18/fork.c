9a7aa9fa66ff68263ca7b72f6956e5f9c3c82c13
void-xiaoxin <ruluy0205@163.com>
2023-10-08 21:57:59 +0800

    lesson18

--- chapter-02/lesson18/fork.c
+++ chapter-02/lesson18/fork.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 
 int main(){
+
+    int num = 10;
     //创建子进程
     pid_t pid = fork();
     //判断是子进程还是父进程
@@ -10,9 +12,19 @@ int main(){
         printf("pid: %d\n", pid);
         printf("I am parent process, pid : %d, ppid : %d\n", getpid(), getppid());
 
+        printf("parent num: %d\n", num);
+        num += 10;
+        printf("parent num += 10: %d\n", num);
+
+
     }
     else if(pid == 0){
         printf("I am child process, pid: %d, ppid: %d\n", getpid(), getppid());
+
+        printf("child num: %d\n", num);
+        num += 100;
+        printf("child num += 100: %d\n", num);
+
     }
 
     for(int i = 0; i < 5; i++){
@@ -20,6 +32,6 @@ int main(){
         sleep(2);
     }
 
-
+ 
     return 0;
 }
