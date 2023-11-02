d6246879bb12d1e3a9d5090ac4a6c27272b117e3
HUAWEI-Ubuntu <ruluy0205@163.com>
2023-11-03 05:32:50 +0800

    producer

--- chapter-03/lesson30/producer.c
+++ chapter-03/lesson30/producer.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+//创建一个互斥量
+pthread_mutex_t mutex;
 
 struct Node{
     int num;
@@ -16,11 +18,16 @@ struct Node* head = NULL;
 void * producer(void * arg){
     //不断地创建新链表添加到节点中
     while(1){
+        pthread_mutex_lock(&mutex);
+
         struct Node * newNode = (struct Node *)malloc(sizeof(struct Node));
         newNode->next = head;
         head = newNode;
         newNode->num = rand() % 1000;
         printf("add node, num = %d, tid : %ld\n", newNode->num, pthread_self());
+
+        pthread_mutex_unlock(&mutex);
+        
         usleep(100);
     }
     return NULL;
@@ -28,19 +35,31 @@ void * producer(void * arg){
 
 void * customer(void * arg){
     while(1){
+        pthread_mutex_lock(&mutex);
+
         //保存头结点的指针
         struct Node * tmp = head;
-        head = head->next;
-        printf("del node, num : %d, tid : %ld\n", tmp->num, pthread_self());
-        free(tmp);
-        usleep(100);
+
+        //判断是否有数据
+        if(head != NULL){
+            head = head->next;
+            printf("del node, num : %d, tid : %ld\n", tmp->num, pthread_self());
+            free(tmp);
+
+            pthread_mutex_unlock(&mutex);
+            usleep(100);
+        }else{
+            pthread_mutex_unlock(&mutex);
+        }
+        
     }
     return NULL;
 }
 
 
-
 int main(){
+
+    pthread_mutex_init(&mutex, NULL);
 
     //创建5个生产者线程，和5个消费者线程
     pthread_t ptids[5], ctids[5];
@@ -54,6 +73,11 @@ int main(){
         pthread_detach(ptids[i]);
         pthread_detach(ctids[i]);
     }
+
+    while(1){
+        sleep(10);
+    }
+    pthread_mutex_destroy(&mutex);
 
     pthread_exit(NULL);
 
