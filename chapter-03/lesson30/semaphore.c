372dbdc96bc34a8902e31ae0ddb468e9d8be6741
HUAWEI-Ubuntu <ruluy0205@163.com>
2023-11-04 04:48:25 +0800

    sem_t

---
+++ chapter-03/lesson30/semaphore.c
@@ -0,0 +1,87 @@
+#define _DEFAULT_SOURCE
+#include <semaphore.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+//创建一个互斥量
+pthread_mutex_t mutex;
+//创建两个信号量
+sem_t psem, csem;
+
+struct Node{
+    int num;
+    struct Node *next;
+};
+
+//头结点
+struct Node* head = NULL;
+
+
+void * producer(void * arg){
+    //不断地创建新链表添加到节点中
+    while(1){
+        sem_wait(&psem);
+        pthread_mutex_lock(&mutex);
+
+        struct Node * newNode = (struct Node *)malloc(sizeof(struct Node));
+        newNode->next = head;
+        head = newNode;
+        newNode->num = rand() % 1000;
+        printf("add node, num = %d, tid : %ld\n", newNode->num, pthread_self());
+
+        pthread_mutex_unlock(&mutex);
+        sem_post(&csem);
+    }
+    return NULL;
+}
+
+void * customer(void * arg){
+    while(1){
+        sem_wait(&csem);
+        pthread_mutex_lock(&mutex);
+
+        //保存头结点的指针
+        struct Node * tmp = head;
+        head = head->next;
+        printf("del node, num : %d, tid : %ld\n", tmp->num, pthread_self());
+        free(tmp);
+
+        pthread_mutex_unlock(&mutex);
+        sem_post(&psem);
+    }
+    return NULL;
+}
+
+
+int main(){
+
+    pthread_mutex_init(&mutex, NULL);
+    sem_init(&psem, 0, 8);
+    sem_init(&csem, 0, 0);
+
+    //创建5个生产者线程，和5个消费者线程
+    pthread_t ptids[5], ctids[5];
+
+    for(int i = 0; i < 5; i++){
+        pthread_create(&ptids[i], NULL, producer, NULL);
+        pthread_create(&ctids[i], NULL, customer, NULL);
+    }
+
+    for(int i = 0; i < 5; i++){
+        pthread_detach(ptids[i]);
+        pthread_detach(ctids[i]);
+    }
+
+    while(1){
+        sleep(10);
+    }
+    pthread_mutex_destroy(&mutex);
+    
+    pthread_exit(NULL);
+
+
+    return 0;
+}
+
